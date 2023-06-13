196473999c0f1e02cc00d3e704d3f45159e0d8ac
Rafael Santos <rafa.05125@gmail.com>
2023-06-13 19:10:09 -0300

    Entrypoint, database provider update

--- src/database.providers.ts
+++ src/database.providers.ts
@@ -19,3 +19,14 @@ export const databaseProviders = [
     },
   },
 ];
+
+export const dataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'docker',
+  database: 'postgres',
+  entities: [__dirname + '/../**/*.entity.js'],
+  synchronize: false,
+});
